536e7ba09f80bf4216dbf08fabce8310b613af12
tjdks <seoulcandlelong@gmail.com>
2025-12-10 17:49:14 +0900

    js 추가

---
+++ load-header.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("afterbegin", data);
+        });
+});
